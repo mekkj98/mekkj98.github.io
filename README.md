859c1255d3c0c354d0cb47fe78bef8275ee9773c
keshav <keshavkumarjha890@gmail.com>
2020-08-21 18:46:21 +0530

    transfer

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# mekkj98.github.io
-[![HitCount](http://hits.dwyl.io/mekkj98/mekkj98githubio.svg)](http://hits.dwyl.io/mekkj98/mekkj98githubio)
+# MY PORTFOLIO
--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,52 +1,130 @@
-"use strict"
-
-var sidenav = {
-    navopener: document.getElementById("toggleNav"),
-    nav: document.getElementById("nav"),
-    navlist: this.nav.querySelectorAll("ul li"),
-
-
-    navClickOpenAndClose: function () {
-        this.navopener.onclick = function () {
-            nav.style.display = "block";
-        }
-        this.navlist[0].onclick = function () {
-            nav.style.display = "none";
-        }
-        for (var lis of this.navlist) {
-            lis.querySelector("a").onclick = function () {
-                nav.style.display = "none";
-                // nav.setAttribute("data-aos", "")
+"use strict";
+/**
+ * All javascript code of hope software.
+ * @package Keshavjha.me
+ * @since 2020
+ * @date 28-02-2020
+ * @author KeshavJha <mekkj98@gmail.com>
+ */
+(function () {
+    /**
+     * @var navbar
+     */
+    var navbar = {
+        alreadyEvent: false,
+        menuElementClickEvent() {
+            if ($(window).width() <= 769) {
+                if (!this.alreadyEvent) {
+                    this.alreadyEvent = true;
+                    $("#header .header-right .navbar ul li a").click(function () {
+                        $("#header .header-right").toggleClass("active");
+                    });
+                }
             }
-        }
-    },
-
-    navlistNoHideNav: function () {
-        for (var lis of this.navlist) {
-            lis.querySelector("a").onclick = function () {
-                nav.style.display = "block";
+        },
+        menuOpener() {
+            $("#navbarOpener").click(function () {
+                $("#header .header-right").toggleClass("active");
+            });
+            this.menuElementClickEvent();
+        },
+        menuOnResize() {
+            this.menuElementClickEvent();
+            if ($(window).width() > 769) {
+                $("#header .header-right").removeClass("active");
+            }
+        },
+        onScroll() {
+            if (window.scrollY <= 400) {
+                $("#header").removeClass("scrolled");
+            } else {
+                $("#header").fadeIn().addClass("scrolled");
             }
         }
     }
 
-}
-
-window.onload = function () {
-    if (window.innerWidth < 720) {
-        sidenav.navClickOpenAndClose();
-        document.getElementById("header").setAttribute("data-aos", "null");
+    /** 
+     * @var tab 
+     * Create methods to handle tab related event
+     */
+    var tab = {
+        init: function (tabIconsSelector, tabContentsSelector) {
+            let tabIcons = $(tabIconsSelector);
+            let tabContents = $(tabContentsSelector);
+            tabIcons.each(function (index, element) {
+                $(element).click(function () {
+                    tabIcons.removeClass("active");
+                    tabContents.removeClass("active").fadeOut();
+                    $("#" + this.getAttribute('data-id')).fadeIn().addClass("active");
+                    $(this).addClass("active");
+                });
+            });
+        }
     };
-}
 
-window.onresize = function () {
-    if (window.innerWidth > 720) {
-        sidenav.nav.style.display = "block";
-        sidenav.navopener.style.display = "none";
-        sidenav.navlistNoHideNav();
-    } else {
-        sidenav.nav.style.display = "none";
-        sidenav.navopener.style.display = "block";
-        sidenav.navClickOpenAndClose();
-        document.getElementById("header").setAttribute("data-aos", "null");
-    }
-}
+    /** 
+     * @var masonary 
+     * Write methods to handle masonary gallery related events
+     */
+    var masonaryGallery = {
+        init: function () {
+            if (jQuery.fn.masonry) {
+                var $grid = jQuery(".grid").masonry();
+                $grid.imagesLoaded().progress(function () {
+                    $grid.masonry("layout");
+                }).always(function () {
+                    $grid.masonry("layout");
+                });
+            }
+        }
+    };
+
+    /**
+     * @var contactForm 
+     * Handle contact form related events
+     */
+    var contactForm = {
+        init: function () {
+            $(".contact-form").submit(function (e) {
+                e.preventDefault();
+                var $form = $(this);
+                $.post($form.attr("action"), $form.serialize()).then(function () {
+                    alert("Thank you!");
+                }).fail(function () {
+                    alert("We are unable to handle the request. Please try again!");
+                });
+                $(".contact-form").each(function () {
+                    this.reset();
+                });
+            });
+        }
+    };
+
+    /** 
+     * @event DOMContentLoaded
+     * run the scripts when doms are loaded */
+    document.addEventListener('DOMContentLoaded', function () {
+        navbar.menuOpener();
+        tab.init(".resume-tabs .tabs-icon-clicker", ".resume-tabs .item");
+        tab.init(".skills-tabs .tabs-icon-clicker", ".skills-tabs .item");
+        masonaryGallery.init();
+        contactForm.init();
+    });
+
+    /**
+     * @event resize
+     * run methods on resize of browser
+     */
+    window.addEventListener("resize", function () {
+        navbar.menuOnResize();
+    })
+
+    /**
+     * @event scoll 
+     * run the scripts when window in scolled down 
+     */
+    document.addEventListener("scroll", () => {
+        navbar.onScroll();
+    });
+
+})();
